d2b0a69e6e4c7ba0e450dbd6fd9dcf7363f71743
agr6014 <agr4106@gmail.com>
2025-11-21 13:27:17 +0000

    Update: Add black bar and attempt to position logo

--- SourceCode/src/App.jsx
+++ SourceCode/src/App.jsx
@@ -4,6 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div style={{
+        backgroundColor: 'black',
+        height: '70px',
+        width: '100%'
+      }}
+      ></div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +26,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
