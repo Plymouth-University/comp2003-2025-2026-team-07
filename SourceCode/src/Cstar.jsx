2053a35ad7526f656aa9c1106a77911e6de21481
agr6014 <agr4106@gmail.com>
2025-11-22 13:45:44 +0000

    Templates for each page
    The screens just need expansion and need to allow interactions

---
+++ SourceCode/src/Cstar.jsx
@@ -0,0 +1,22 @@
+import logo from './Oshen_logo.png';
+import './Cstar.css';
+
+function Cstar() {
+  return (
+    <div className='dashboard_container'>
+      <div className="black_bar">
+        <div className='logo'>
+          <img src={logo} alt="Oshen Logo" className='logo'/>
+        </div>
+        <div className='buttons'>
+          <button>Cstars</button>
+          <button>Geofences</button>
+          <button>User support</button>
+        </div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default Cstar;
---
+++ SourceCode/src/Geofences.jsx
@@ -0,0 +1,22 @@
+import logo from './Oshen_logo.png';
+import './Geofences.css';
+
+function Geofences() {
+  return (
+    <div className='dashboard_container'>
+      <div className="black_bar">
+        <div className='logo'>
+          <img src={logo} alt="Oshen Logo" className='logo'/>
+        </div>
+        <div className='buttons'>
+          <button>Cstars</button>
+          <button>Geofences</button>
+          <button>User support</button>
+        </div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default Geofences;
