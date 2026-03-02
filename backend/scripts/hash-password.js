d3f845f8f48dd517efda4fea15b5486973cecd41
alvin-dev32 <alvinogbeifun6@gmail.com>
2026-03-02 11:28:50 +0000

    compound alert update

--- backend/scripts/hash-password.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function hashPassword(password) {
-  const hash = await bcrypt.hash(password, 10);
-  console.log('Hashed password:', hash);
-}
-
-// Replace with the actual admin password
-hashPassword('emperorpinguoshen');
